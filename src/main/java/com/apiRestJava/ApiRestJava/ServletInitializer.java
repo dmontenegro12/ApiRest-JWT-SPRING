b41c1553289b38dd8e136481b1b027fb8a5ed568
dmontenegro12 <diablicho-david@hotmail.com>
2023-09-28 11:08:20 -0500

    primer commit

---
+++ src/main/java/com/apiRestJava/ApiRestJava/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.apiRestJava.ApiRestJava;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiRestJavaApplication.class);
+	}
+
+}
