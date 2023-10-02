6f8cfbfb91ad6bcd42febbe341a42a4c06433fb0
dmontenegro12 <diablicho-david@hotmail.com>
2023-10-02 10:05:52 -0500

    Segunda Modificacion total

---
+++ src/main/java/com/apiRestJava/ApiRestJava/models/Usuario.java
@@ -0,0 +1,40 @@
+package com.apiRestJava.ApiRestJava.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "usuarios")
+@ToString @EqualsAndHashCode
+public class Usuario {
+
+    @Getter @Setter @Column(name = "id")
+    @Id
+    private Long id;
+
+    @Getter @Setter @Column(name = "nombre")
+    private String nombre;
+
+    @Getter @Setter @Column(name = "apellido")
+    private String Apellido;
+
+    @Getter @Setter @Column(name = "email")
+    private String email;
+
+    @Getter @Setter @Column(name = "telefono")
+    private String telefono;
+
+    @Getter @Setter @Column(name = "password")
+    private String password;
+
+
+
+    
+
+}
